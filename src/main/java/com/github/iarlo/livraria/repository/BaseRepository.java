f7603576171959a15023995805449f5adb4b49c7
Bruno Sá <53959303+iarlo@users.noreply.github.com>
2025-08-21 10:52:16 -0300

    feat(LivroRepository): Adiciona novos métodos para busca de dados

--- src/main/java/com/github/iarlo/livraria/repository/BaseRepository.java
+++ src/main/java/com/github/iarlo/livraria/repository/BaseRepository.java
@@ -4,11 +4,23 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class BaseRepository<T, U> {
+public abstract class BaseRepository<T> {
 
-    protected Map<U, T> dados = new HashMap<>();
+    protected Map<Integer, T> dados = new HashMap<>();
 
     public List<T> buscarTodos() {
         return List.copyOf(dados.values());
+    }
+
+    public T buscarPorId(int id) {
+        return dados.get(id);
+    }
+
+    public void editar(int id, T objeto) {
+        dados.put(id, objeto);
+    }
+
+    public void excluir(int id) {
+        dados.remove(id);
     }
 }
--- src/main/java/com/github/iarlo/livraria/repository/LivroRepository.java
+++ src/main/java/com/github/iarlo/livraria/repository/LivroRepository.java
@@ -2,6 +2,6 @@ package com.github.iarlo.livraria.repository;
 
 import com.github.iarlo.livraria.model.Livro;
 
-public class LivroRepository extends BaseRepository<String, Livro> {
+public class LivroRepository extends BaseRepository<Livro> {
 
 }
