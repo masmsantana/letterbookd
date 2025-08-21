a661e21a368582474089e109d938c21121422499
Bruno Sá <53959303+iarlo@users.noreply.github.com>
2025-08-21 10:12:04 -0300

    feat(Livro): Adiciona o modelo de livro

---
+++ src/main/java/com/github/iarlo/livraria/model/Livro.java
@@ -0,0 +1,28 @@
+package com.github.iarlo.livraria.model;
+
+public class Livro extends BaseModel {
+
+    private String titulo;
+    private String autor;
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
