5d789f0fbc8d64d5501260ceae81120c4461d0ea
Bruno Sá <53959303+iarlo@users.noreply.github.com>
2025-08-22 22:25:38 -0300

    refactor(InicioController): Removidos métodos ainda não implementados

--- src/main/java/com/github/iarlo/livraria/controller/InicioController.java
+++ src/main/java/com/github/iarlo/livraria/controller/InicioController.java
@@ -4,32 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import com.github.iarlo.livraria.model.Livro;
-import com.github.iarlo.livraria.service.LivroService;
+import com.github.iarlo.livraria.service.LivrariaService;
 import java.util.List;
 
 @Controller
-
 public class InicioController {
 
-    private final LivroService livroService;
-
-    //Injeção do service
-    public InicioController(LivroService livroService) {
-        this.livroService = livroService;
-    }
+    private final LivrariaService livrariaService = new LivrariaService();
 
     @GetMapping("/")
-        public String index (Model model) {
-            List<Livro> livroslidos = livroService.buscarLivrosLidos();
-            List<Livro> livroslista = livroService.buscarLivrosLista();
-            List<Livro> livrosaicionados = livroService.buscarLivrosAdicionados();
+    public String index(Model model) {
+        List<Livro> livroslidos = livrariaService.listarTodos();
+        List<Livro> livroslista = livrariaService.listarTodos();
+        List<Livro> livrosaicionados = livrariaService.listarTodos();
 
-            model.addAttribute("livroslidos", livroslidos);
-            model.addAttribute("livroslista", livroslista);
-            model.addAttribute("livrosaicionados", livrosaicionados);
+        model.addAttribute("livroslidos", livroslidos);
+        model.addAttribute("livroslista", livroslista);
+        model.addAttribute("livrosadicionados", livrosaicionados);
 
-            return "index";
+        return "index";
 
-        }
+    }
 
 }
