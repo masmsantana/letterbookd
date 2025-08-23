0c5e46db8e51ce85cd0f2d0dc05eaea34b741d05
Caio <118756888+caiomagn0@users.noreply.github.com>
2025-08-23 08:52:57 -0300

    Model - Classe Review.java

---
+++ src/main/java/com/github/iarlo/livraria/model/Review.java
@@ -0,0 +1,52 @@
+package com.github.iarlo.livraria.model;
+
+import java.time.LocalDate;
+
+    public class Review {
+        private String texto;
+        private String autor;
+        private LocalDate dataDoPost;
+        private int nota;
+
+        
+        public String getTexto() {
+            return texto;
+        }
+        
+        public void setTexto(String texto) {
+            this.texto = texto;
+        }
+        
+        public String getAutor() {
+            return autor;
+        }
+        
+        public void setAutor(String autor) {
+            this.autor = autor;
+        }
+        
+        public LocalDate getDataDoPost() {
+            return dataDoPost;
+        }
+
+        public void setDataDoPost(LocalDate dataDoPost) {
+            this.dataDoPost = dataDoPost;
+        }
+
+        public int getNota() {
+            return nota;
+        }
+        
+        public void setNota(int nota) {
+            if (nota < 0 || nota > 10){
+                throw new IllegalArgumentException("A nota deve estar entre 0 e 10.");
+            }
+        }
+
+        public Review(String texto, String autor, LocalDate dataDoPost, int nota){
+            this.texto = texto;
+            this.autor = autor;
+            this.dataDoPost = dataDoPost;
+            setNota(nota);
+        }
+    }
