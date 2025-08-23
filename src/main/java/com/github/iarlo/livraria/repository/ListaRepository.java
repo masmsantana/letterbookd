b61270d619183405963abba1cf83b3348b17205c
Caio <118756888+caiomagn0@users.noreply.github.com>
2025-08-23 17:52:12 -0300

    Adição dos métodos da ListaRepository

--- src/main/java/com/github/iarlo/livraria/repository/ListaRepository.java
+++ src/main/java/com/github/iarlo/livraria/repository/ListaRepository.java
@@ -1,7 +1,7 @@
 package com.github.iarlo.livraria.repository;
 
 import com.github.iarlo.livraria.model.Lista;
-
+import com.github.iarlo.livraria.model.Livro;
 /**
  * Implementa o padrão de projeto singleton para garantir que a classe seja
  * criada apenas uma vez. Isso é bom pra gente que vai armazenar dados na
@@ -10,24 +10,42 @@ import com.github.iarlo.livraria.model.Lista;
  * Referência:
  * https://refactoring.guru/pt-br/design-patterns/singleton/java/example#example-1
  */
-public class ListaRepository extends BaseRepository<Lista> {
-
+public class ListaRepository {
+    
     private static ListaRepository instancia;
-
+    private final Lista lista = new Lista();
+  /**
+     * O constructor privado serve pra impedir que a classe seja acessada
+     * diretamente
+     */
+    private ListaRepository(){}
     /**
      * Se a instancia já tiver sido definida, retorna ela, se não, cria uma nova
      */
-    public static ListaRepository pegarInstancia() {
-        if (instancia == null) {
+    public static ListaRepository getInstance(){
+        if(instancia == null){
             instancia = new ListaRepository();
         }
         return instancia;
     }
 
-    /**
-     * O constructor privado serve pra impedir que a classe seja acessada
-     * diretamente
-     */
-    private ListaRepository() {
+    public void adicionarLivroLido(Livro livro) {
+        lista.addLivroLido(livro);
+    }
+
+    public void adicionarLivroNaLista(Livro livro) {
+        lista.addLivroLista(livro);
+    }
+
+    public void removeLivroLido(Livro livro) {
+        lista.removeLivroLido(livro);
+    }
+
+    public void removeLivroLista(Livro livro) {
+        lista.removeLivroLista(livro);
+    }
+
+    public Lista getLista() {
+        return lista;
     }
 }
