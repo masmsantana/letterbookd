bff5c8479af1a41102deaacbfaab0ded5484e568
Caio <118756888+caiomagn0@users.noreply.github.com>
2025-08-23 08:57:26 -0300

    Model Livro adição do Atributo Descrição e Review

--- src/main/java/com/github/iarlo/livraria/model/Livro.java
+++ src/main/java/com/github/iarlo/livraria/model/Livro.java
@@ -4,10 +4,13 @@ public class Livro extends BaseModel {
 
     private String titulo;
     private String autor;
-
-    public Livro(String titulo, String autor) {
+    private String descricao;
+    private List<Review> reviews = new ArrayList<>();
+    
+    public Livro(String titulo, String autor, String descricao) {
         this.titulo = titulo;
         this.autor = autor;
+        this.descricao = descricao;
     }
 
     public String getTitulo() {
@@ -24,5 +27,21 @@ public class Livro extends BaseModel {
 
     public void setAutor(String autor) {
         this.autor = autor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public List<Review> getReview() {
+        return reviews;
+    }
+
+    public void addReview(Review review) {
+    this.reviews.add(review);
     }
 }
